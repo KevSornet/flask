3919bdc07ba99be52acf4309644d5994f8fd1b92
KevSornet <kevin.sornet@obf.ateneo.edu>
2021-12-01 16:19:39 +0800

    added initial files

---
+++ digitalcafe/database.py
@@ -0,0 +1,40 @@
+products = {
+    100: {"name":"Americano","price":125},
+    200: {"name":"Brewed Coffee","price":100},
+    300: {"name":"Cappuccino","price":120},
+    400: {"name":"Espresso","price":120}
+}
+
+branches = {
+    1: {"name":"Katipunan"},
+    2: {"name":"Tomas Morato"},
+    3: {"name":"Eastwood"},
+    4: {"name":"Tiendesitas"},
+    5: {"name":"Arcovia"},
+}
+
+def get_product(code):
+    return products[code]
+
+def get_products():
+    product_list = []
+
+    for i,v in products.items():
+        product = v
+        product.setdefault("code",i)
+        product_list.append(product)
+
+    return product_list
+
+def get_branch(code):
+    return branches[code]
+
+def get_branches():
+    branch_list = []
+
+    for i,v in branches.items():
+        branch = v
+        branch.setdefault("code",i)
+        branch_list.append(branch)
+
+    return branch_list
